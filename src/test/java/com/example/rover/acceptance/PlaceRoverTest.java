5f52d0971e657da917d818d08c7e78d219a27a18
Andrzej Prochyra <andrzej@prochyra.io>
2026-01-05 16:23:18 +0000

    test: add "good enough" manual acceptance test

---
+++ src/test/java/com/example/rover/acceptance/PlaceRoverTest.java
@@ -0,0 +1,18 @@
+package com.example.rover.acceptance;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class PlaceRoverTest {
+
+    @Test
+    @Tag("Manual")
+    void rover_is_placed_at_0_0() {
+        // Given we are on Fake Mars
+        // When the Sky Crane places the Rover
+        // Then the Rover is at (0, 0) N
+        fail("This is a manual test to verify rover placement at initial position");
+    }
+}
