787b09867d643538707fa86570e9e39669e9526c
Rob Ratcliffe <rlr@tcliffe.dev>
2026-04-24 12:34:04 -0500

    incomplete feature (failing test): rover placed at given position

--- src/test/java/com/example/rover/acceptance/PlaceRoverTest.java
+++ src/test/java/com/example/rover/acceptance/PlaceRoverTest.java
@@ -1,9 +1,7 @@
 package com.example.rover.acceptance;
 
 import org.htmlunit.WebClient;
-import org.htmlunit.html.HtmlBody;
-import org.htmlunit.html.HtmlForm;
-import org.htmlunit.html.HtmlPage;
+import org.htmlunit.html.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -40,5 +38,25 @@ class PlaceRoverTest {
         HtmlBody body = roverPositionPage.getBody();
 
         assertThat(body.asNormalizedText()).contains("(0, 0) N");
+    }
+
+    @Test
+    void rover_placed_at_given_position() throws IOException {
+        HtmlPage indexPage = webClient.getPage("/");
+        HtmlForm htmlForm = indexPage.getForms().getFirst();
+        HtmlInput xCoordinateInput = htmlForm.getInputByName("x-coordinate");
+        HtmlInput yCoordinateInput = htmlForm.getInputByName("y-coordinate");
+        xCoordinateInput.type("4");
+        yCoordinateInput.type("5");
+
+        HtmlSelect direction = htmlForm.getSelectByName("direction");
+        HtmlOption eastOption = direction.getOptionByValue("E");
+        direction.setSelectedAttribute(eastOption, true);
+
+        HtmlPage roverPositionPage = htmlForm.getInputByValue("Place Rover").click();
+
+        HtmlBody body = roverPositionPage.getBody();
+
+        assertThat(body.asNormalizedText()).contains("(4, 5) E");
     }
 }
