ee27c0cd358d339da339cf38ab57d0b0e609d82b
Rob Ratcliffe <20655651+rlratcliffe@users.noreply.github.com>
2026-06-12 12:06:12 -0500

    test: remove superfluous test

--- src/test/java/com/example/rover/core/domain/RoverTurningLeftTest.java
+++ src/test/java/com/example/rover/core/domain/RoverTurningLeftTest.java
@@ -1,21 +1,12 @@
 package com.example.rover.core.domain;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RoverTurningLeftTest {
-
-    @Test
-    void rover_turns_left_when_pointing_north() {
-        Rover rover = new Rover(new Position(new Coordinates(1, 2), Direction.NORTH));
-
-        Rover turnedRover = rover.turnLeft();
-
-        assertThat(turnedRover)
-                .isEqualTo(new Rover(new Position(new Coordinates(1, 2), Direction.WEST)));
-    }
 
     @Test
     void rover_turns_left_when_pointing_west() {
@@ -25,5 +16,16 @@ class RoverTurningLeftTest {
 
         assertThat(turnedRover)
                 .isEqualTo(new Rover(new Position(new Coordinates(1, 2), Direction.SOUTH)));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"NORTH, WEST"})
+    void rover_turns_left(Direction start, Direction expected) {
+        Rover rover = new Rover(new Position(new Coordinates(1, 2), start));
+
+        Rover turnedRover = rover.turnLeft();
+
+        assertThat(turnedRover)
+                .isEqualTo(new Rover(new Position(new Coordinates(1, 2), expected)));
     }
 }
