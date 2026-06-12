31c2ca3b05204fed538e8aac42c1c82ce63be218
Andrzej Prochyra <andrzej@prochyra.io>
2026-06-12 18:11:30 +0100

    test: convert test to parameterized test

--- src/test/java/com/example/rover/core/domain/RoverTurningLeftTest.java
+++ src/test/java/com/example/rover/core/domain/RoverTurningLeftTest.java
@@ -1,6 +1,5 @@
 package com.example.rover.core.domain;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -8,18 +7,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class RoverTurningLeftTest {
 
-    @Test
-    void rover_turns_left_when_pointing_west() {
-        Rover rover = new Rover(new Position(new Coordinates(1, 2), Direction.WEST));
-
-        Rover turnedRover = rover.turnLeft();
-
-        assertThat(turnedRover)
-                .isEqualTo(new Rover(new Position(new Coordinates(1, 2), Direction.SOUTH)));
-    }
-
     @ParameterizedTest
-    @CsvSource({"NORTH, WEST"})
+    @CsvSource({
+            "NORTH, WEST",
+            "WEST, SOUTH"
+    })
     void rover_turns_left(Direction start, Direction expected) {
         Rover rover = new Rover(new Position(new Coordinates(1, 2), start));
 
